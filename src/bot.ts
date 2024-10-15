e6608cd9380bff07ef77786f91bbba492f20d711
Mintesnot M <jamymarcoss47@gmail.com>
2024-10-15 14:20:56 +0300

    Refactor bot.ts to add a health check route

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,29 @@
 import TelegramBot from "node-telegram-bot-api";
 import { handleIncomingMessage } from "./handlers/messageHandler";
 import dotenv from "dotenv";
+import http from "http";
 
 // Load environment variables
 dotenv.config();
+
+/* a sample route for health check
+// Create a server object
+*/
+const server = http.createServer((req: any, res: any) => {
+  // Set the response header with HTTP status and Content-Type
+  res.writeHead(200, { "Content-Type": "text/plain" });
+
+  // Send a response message
+  res.end("Hello, world! This is a simple Node.js server.\n");
+});
+
+// Define the port to listen on
+const port = 3000;
+
+// Start the server
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
 const botToken = process.env.TELEGRAM_BOT_TOKEN;
 
